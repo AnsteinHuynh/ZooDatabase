5e5d28f5d8dd28e2a45b08f963b8b8a9e3b909c9
AnsteinHuynh <53374922+AnsteinHuynh@users.noreply.github.com>
2022-04-01 22:20:44 -0500

    Add files via upload

---
+++ ZooDatabase-main/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "cosc3380-zoo.database.windows.net";
+$dbUsername = "dhphan3";
+$dbPassword = "K7EY2kh@ri*oJH9";
+$dbName = "ZooDatabaseDump";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
