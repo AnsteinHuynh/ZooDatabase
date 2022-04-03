28a047a442ebb3e22701655a9a79c8371862265c
AnsteinHuynh <53374922+AnsteinHuynh@users.noreply.github.com>
2022-04-03 18:34:19 -0500

    Delete dbh.php

--- dbh.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "cosc3380-zoo.database.windows.net";
-$dbUsername = "dhphan3";
-$dbPassword = "K7EY2kh@ri*oJH9";
-$dbName = "ZooDatabaseDump";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
